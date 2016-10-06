cf3021e63b8cac4f1aa79fc070822e731029686b
Josh Mcguigan <joshmcg88@gmail.com>
2016-10-05 21:51:08 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Need to modify inverter class to account for possibility of adding deadtime or other local inverter effects
+        // Need to modify inverter class to account for possibility of adding deadtime or other local inverter level effects
 
         Simulator sim = new Simulator();
-
+        
         for (int i = 0; i < 20; i++) {
             System.out.println("Step " + i);
             sim.simStep();
