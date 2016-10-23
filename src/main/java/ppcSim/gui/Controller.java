0f0f3d5d59e26110addccaea90f121c18952e8a5
JoshMcguigan <joshmcg88@gmail.com>
2016-10-23 09:00:31 -0700

    Added plant power simulation length control from GUI

--- src/main/java/ppcSim/gui/Controller.java
+++ src/main/java/ppcSim/gui/Controller.java
@@ -9,7 +9,6 @@ import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Slider;
-import javafx.scene.text.Text;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,49 +22,35 @@ public class Controller {
     // SIMULATION CONFIGURATION PARAMETERS
     private static final int invQuantity = 20; // quantity of inverters in simulation
     private static final double maxIrr = 1500; // maximum irradiance put out by sun (W/m^2)
-    private static final double simLength = 600; // simulation time in seconds
+    private static double simLength = 600; // simulation time in seconds
     private static final double simStepSize = .5; // simulation step size in seconds
     private static final double controllerExecutionRate = 6; // Rate of power plant controller execution, in seconds
     private static final double invMaxPower = 2.2; // maximum power per inverter (MW)
     private static final double invMaxIrr = 1400; // irradiance required by inverters to output max power
     private static final double invVariability = 0.5; // variability in inverter power when limited by set point (% of maximum power)
-    private static double plantPowerSetPoint = 20; // plant power set point (MW)
+    private static double plantPowerSetPoint = 17.5; // plant power set point (MW)
     private static final double substationDeadTime = 2.0; // time delay in substation power measurement (seconds)
 
+    private static final int secondsPerMinute = 60;
 
-    private static final int steps = (int)(simLength/simStepSize);
+    private static int steps;
     private static List<AbstractController> controllers;
     private static Inverter[] inverter;
     private static Substation substation;
 
     @FXML private LineChart<Double, Double> chart;
     @FXML private Slider sliderPowerSetPoint;
+    @FXML private Slider sliderSimLength;
 
     @FXML
     protected void initialize() {
 
-        sliderPowerSetPoint.setValue(plantPowerSetPoint);
-        sliderPowerSetPoint.setMin(0.0);
-        sliderPowerSetPoint.setMax(40.0);
-        sliderPowerSetPoint.setShowTickLabels(true);
-        sliderPowerSetPoint.setShowTickMarks(true);
-        sliderPowerSetPoint.setMajorTickUnit(10);
-        sliderPowerSetPoint.setMinorTickCount(5);
-        sliderPowerSetPoint.setBlockIncrement(1);
-
-        // Listen for Slider value changes
-        sliderPowerSetPoint.valueProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observable,
-                                Number oldValue, Number newValue) {
-
-                plantPowerSetPoint = (double)newValue;
-            }
-        });
+        setupUIElements();
 
         runSim();
 
     }
+
 
 
     @FXML protected void runSim(ActionEvent event) {
@@ -75,6 +60,9 @@ public class Controller {
     private void runSim(){
 
         System.out.println("Simulation starting");
+
+        // Calculate number of steps in simulation
+        int steps = (int)(simLength/simStepSize);
 
         // Instantiate simulation objects
         inverter = new Inverter[invQuantity];
@@ -119,7 +107,7 @@ public class Controller {
         LineChart.Series<Double, Double> setPoint = new LineChart.Series<Double, Double>();
         setPoint.setName("Set Point");
         for (int i = 0; i < plantData[0].length; i++) {
-            Double x = plantData[0][i].timeStamp;
+            Double x = plantData[0][i].timeStamp / secondsPerMinute;
             Double y = plantData[0][i].plantSetPoint;
             setPoint.getData().add(new XYChart.Data<Double,Double>(x,y));
         }
@@ -132,7 +120,7 @@ public class Controller {
             plantOutput.setName(controllerNames[controller]);
 
             for (int i = 0; i < plantData[controller].length; i++) {
-                Double x = plantData[0][i].timeStamp;
+                Double x = plantData[0][i].timeStamp / secondsPerMinute;
                 Double y = plantData[controller][i].plantPowerOutput;
                 plantOutput.getData().add(new XYChart.Data<Double,Double>(x,y));
             }
@@ -143,6 +131,46 @@ public class Controller {
         chart.setCreateSymbols(false);
         chart.setData(lineChartData);
         chart.createSymbolsProperty();
+    }
+
+    private void setupUIElements(){
+        // Set up sliders
+        sliderPowerSetPoint.setValue(plantPowerSetPoint);
+        sliderPowerSetPoint.setMin(0.0);
+        sliderPowerSetPoint.setMax(invQuantity * invMaxPower);
+        sliderPowerSetPoint.setShowTickLabels(true);
+        sliderPowerSetPoint.setShowTickMarks(true);
+        sliderPowerSetPoint.setMajorTickUnit(10);
+        sliderPowerSetPoint.setMinorTickCount(5);
+        sliderPowerSetPoint.setBlockIncrement(1);
+
+        sliderSimLength.setValue(simLength/secondsPerMinute);
+        sliderSimLength.setMin(0.0);
+        sliderSimLength.setMax(60);
+        sliderSimLength.setShowTickLabels(true);
+        sliderSimLength.setShowTickMarks(true);
+        sliderSimLength.setMajorTickUnit(10);
+        sliderSimLength.setMinorTickCount(5);
+        sliderSimLength.setBlockIncrement(1);
+
+        // Listen for slider value changes
+        sliderPowerSetPoint.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable,
+                                Number oldValue, Number newValue) {
+
+                plantPowerSetPoint = (double)newValue;
+            }
+        });
+        sliderSimLength.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable,
+                                Number oldValue, Number newValue) {
+
+                // Allow sim length down to 1 minute only
+                simLength = Math.max((double)newValue, 1) * 60;
+            }
+        });
     }
 
 }
