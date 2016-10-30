8661ff31a91d2b9adffcdaed962efbf57f5caff0
JoshMcguigan <joshmcg88@gmail.com>
2016-10-30 13:14:07 -0700

    Modified open loop controller to calculate a ramped power set point

--- src/main/java/ppcSim/sim/OpenLoopController.java
+++ src/main/java/ppcSim/sim/OpenLoopController.java
@@ -2,24 +2,37 @@ package ppcSim.sim;
 
 import java.util.Arrays;
 
+public class OpenLoopController extends AbstractTimeBasedController{
 
-
-
-public class OpenLoopController extends AbstractController{
+    private Double rampedPlantPowerSetPoint;
 
     public OpenLoopController(ControllerSettings settings, int invQuantity, double invPowerMax) {
-        super(invQuantity, invPowerMax);
+        super(settings, invQuantity, invPowerMax);
     }
 
-    public double[] getPowerSetPoints(double plantPowerSetPoint, double currentPlantPower, double[] currentInverterPower, double timeStamp){
+    public double[] executeController(double plantPowerSetPoint, double currentPlantPower, double[] currentInverterPower, double timeStamp){
 
-
+        calculateRampedPlantPowerSetPoint(plantPowerSetPoint);
 
         // Simple open loop control of plant power output
-        double calculatedPowerSetPoint = ( plantPowerSetPoint / plantPowerMax ) * 100;
+        double calculatedPowerSetPoint = ( rampedPlantPowerSetPoint / plantPowerMax ) * 100;
         Arrays.fill(powerSetPoints, Math.min( Math.max(calculatedPowerSetPoint, minPowerSetPoint) , maxPowerSetPoint) );
 
         return powerSetPoints;
+
+    }
+
+    private void calculateRampedPlantPowerSetPoint(double plantPowerSetPoint){
+        if (rampedPlantPowerSetPoint == null){
+            rampedPlantPowerSetPoint = plantPowerSetPoint;
+        } else{
+            if (plantPowerSetPoint > rampedPlantPowerSetPoint){
+                rampedPlantPowerSetPoint = Math.min(plantPowerSetPoint, rampedPlantPowerSetPoint + maxStepSizePerExecution);
+            }
+            if (plantPowerSetPoint < rampedPlantPowerSetPoint){
+                rampedPlantPowerSetPoint = Math.max(plantPowerSetPoint, rampedPlantPowerSetPoint - maxStepSizePerExecution);
+            }
+        }
 
     }
 
