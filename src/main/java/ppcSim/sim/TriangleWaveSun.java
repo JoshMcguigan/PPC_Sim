10b37efe789e927c4016f8da5a0771a738430fb4
joshmcguigan <joshmcg88@gmail.com>
2017-01-10 17:29:07 -0700

    Updated triangle wave irradiance pattern to be more responsive to settings changes during operation

--- src/main/java/ppcSim/sim/TriangleWaveSun.java
+++ src/main/java/ppcSim/sim/TriangleWaveSun.java
@@ -5,16 +5,17 @@ public class TriangleWaveSun extends AbstractSun{
     private double changeRate; // rate of change, in units of (W/m^2)/sec
     private double irradiance;
     private double[] noiseyIrr;
+    private boolean increasing; // boolean tracks whether the triangle wave is increasing or decreasing
 
     public TriangleWaveSun(SunSettings settings, int invQuantity){
 
         super(settings, invQuantity);
 
-        changeRate = 4 * settings.range / settings.period;
-
         irradiance = settings.baseIrr - settings.range;
 
         noiseyIrr = new double[invQuantity];
+
+        increasing = true;
     }
 
     @Override
@@ -22,19 +23,25 @@ public class TriangleWaveSun extends AbstractSun{
 
         double timeDelta = getTimeDeltaAndUpdateTimeStamp(timeStamp);
 
-        // Change irradiance baseline by step size
-        irradiance += changeRate * timeDelta;
+        changeRate = 4 * settings.range / settings.period;
 
+        // Change irradiance baseline by step size
+        if (increasing) {
+            irradiance += changeRate * timeDelta;
+        } else{
+            irradiance -= changeRate * timeDelta;
+        }
         // If irradiance is too high or low, limit the irradiance value and modify sign of step size
         if ( (irradiance > settings.maxIrr) || (irradiance > settings.baseIrr + settings.range) ){
             irradiance = Math.min(settings.maxIrr, settings.baseIrr + settings.range);
-            changeRate *= -1;
+            increasing = !increasing;
         }
 
         if ( (irradiance < 0 ) || (irradiance < settings.baseIrr - settings.range) ){
             irradiance = Math.max(0.0, settings.baseIrr - settings.range);
-            changeRate *= -1;
+            increasing = !increasing;
         }
+
 
         // Add a little noise to the irradiance value for each inverter
         for (int i = 0; i < noiseyIrr.length; i++) {
