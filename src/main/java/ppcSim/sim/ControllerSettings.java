5268c96cb54f33dfb710d44d14d86cbec0abe097
joshmcguigan <joshmcg88@gmail.com>
2017-01-11 14:13:41 -0700

    Controller configuration value modifications

--- src/main/java/ppcSim/sim/ControllerSettings.java
+++ src/main/java/ppcSim/sim/ControllerSettings.java
@@ -4,7 +4,7 @@ public class ControllerSettings {
 
     public double executionRate = 6; // Rate of power plant controller execution, in seconds
     public double targetRampRate = 25; // in % of full plant output per minute
-    public double deadBand = .2; // control dead band in MW deadband exists only on the low side of the plant set point
+    public double deadBand = 0; // control dead band in MW deadband exists only on the low side of the plant set point
     public double setPointOffset = 0; // allows the controller to target a set point which is below the actual set point in order to reduce overproduction events (MW), can be negative to move deadband above actual set point
 
 
